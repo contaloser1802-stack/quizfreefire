c4443f9d8347a48dc5672640beee7c69ab4398ce
contaloser1802-stack <contaloser1802@gmail.com>
2026-01-07 21:34:25 -0300

    Update verificar.php

--- parte2/verificar.php
+++ parte2/verificar.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-$secretToken = 'sk_live_69b0ed89aaa545ef5e67bfcef2c3e0c4';
+$secretToken = 'sk_live_24f5f78fec2d518816353b2f44a5465b';
 
 $utmifyApiKey = 'oPNL0wMgnj3jgDWfRa5WTGvkY1zZE5tkagKA';
 
